117fb0bf932f03604feafbd68f22227114ad2430
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2023-12-29 21:21:12 +0800

    implement firebase on comment section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let currentIndex = 0;
+let apiUrl = "https://comment-folio-default-rtdb.asia-southeast1.firebasedatabase.app/comments.json"
 
 function showTestimonial(index) {
   const testimonials = document.querySelectorAll(".testimonial");
@@ -22,50 +23,29 @@ function changeTestimonial(direction) {
 
 showTestimonial(currentIndex);
 
-const commentSection = [
-  { 
-    name: "Odrunia, Mark", 
-    comment: `Great goals, hoping you achieve all of them someday!`, 
-    dateTime: "10/22/2023 8:40:02 PM" 
-  },
-  { 
-    name: "Dimasacat, Andronicus", 
-    comment: `I like how your goals will change your 
-              future and influence others`, 
-    dateTime: "10/22/2023 7:53:02 AM" 
-  },
-  { 
-    name: "Go, Harrel C", 
-    comment: `More jams to come before we finish our degree. 
-              Good luck on your journey!`, 
-    dateTime: "10/23/2023 3:51:02 AM" 
-  },
-  { 
-    name: "Galope, Jhean Khendrick", 
-    comment: `Nice work broski! Keep it up, 
-              and you'll be where you dream of in no time.`, 
-    dateTime: "10/22/2023 6:51:02 AM" 
-  }
-];
+document.addEventListener("DOMContentLoaded", () => {
+  sortComments();
 
-sortComments();
+  document.querySelector("#name")
+    .addEventListener("input", updateButtonState);
+  document.querySelector("#comment")
+    .addEventListener("input", updateButtonState);
+  document.querySelector("#add_comment_btn")
+    .addEventListener("click", addComment);
+  document.querySelector("#sort")
+    .addEventListener("change", sortComments);
 
-document.querySelector("#name")
-  .addEventListener("input", updateButtonState);
-document.querySelector("#comment")
-  .addEventListener("input", updateButtonState);
+  displayComments();
+});
 
 function updateButtonState() {
   const nameInput = document.querySelector("#name");
   const commentInput = document.querySelector("#comment");
   const addCommentBtn = document.querySelector("#add_comment_btn");
 
-  addCommentBtn.disabled 
-  = !nameInput.value.trim() || !commentInput.value.trim();
+  addCommentBtn.disabled = 
+    !nameInput.value.trim() || !commentInput.value.trim();
 }
-
-document.querySelector("#add_comment_btn")
-  .addEventListener("click", addComment);
 
 function addComment() {
   const nameInput = document.querySelector("#name");
@@ -76,50 +56,118 @@ function addComment() {
   const comment = commentInput.value.trim();
   const dateTime = new Date().toLocaleString();
 
-  if (!name.length || !comment.length) {
-      addCommentBtn.disabled = true;
-      return;
+  if (!name.length || !comment.length || addCommentBtn.disabled) {
+    return;
   }
 
-  const newComment = { name, comment, dateTime };
-  commentSection.push(newComment);
-  displayComments();
-  nameInput.value = "";
-  commentInput.value = "";
   addCommentBtn.disabled = true;
+
+  const requestBody = {
+    name: name,
+    comment: comment,
+    dateTime: dateTime
+  };
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestBody)
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(() => {
+      nameInput.value = "";
+      commentInput.value = "";
+      displayComments();
+    })
+    .catch((error) => {
+      console.error("Error adding comment:", error.message);
+    })
+    .finally(() => {
+      addCommentBtn.disabled = false;
+    });
 }
 
 function displayComments() {
   const commentsContainer = document.querySelector("#comments_dynamic");
   commentsContainer.innerHTML = "";
 
-  commentSection.forEach((comment) => {
-      const commentDiv = document.createElement("div");
-      commentDiv.classList.add("comments_dynamic");
-      commentDiv.innerHTML =
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      for (const commentId in data) {
+        const comment = data[commentId];
+        const commentDiv = document.createElement("div");
+        commentDiv.classList.add("comments_dynamic");
+        commentDiv.innerHTML =
           `<p>
               <strong>${comment.name}</strong> - ${comment.dateTime}
           </p>
           <p>
               <em>${comment.comment}</em>
           </p>`;
-      commentsContainer.appendChild(commentDiv);
-  });
+        commentsContainer.appendChild(commentDiv);
+      }
+    })
+    .catch((error) => {
+      console.error("Error retrieving comments:", error.message);
+    });
 }
 
 function sortComments() {
   const sortOption = document.querySelector("#sort").value;
 
-  if (sortOption === "newest") {
-      commentSection.sort((a, b) =>
-          new Date(b.dateTime) - new Date(a.dateTime)
-      );
-  } else if (sortOption === "oldest") {
-      commentSection.sort((a, b) =>
-          new Date(a.dateTime) - new Date(b.dateTime)
-      );
-  }
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const commentsArray = Object.values(data);
 
-  displayComments();
+      if (sortOption === "newest") {
+        commentsArray.sort((a, b) => 
+          new Date(b.dateTime) - new Date(a.dateTime));
+      } else if (sortOption === "oldest") {
+        commentsArray.sort((a, b) => 
+          new Date(a.dateTime) - new Date(b.dateTime));
+      }
+
+      displaySortedComments(commentsArray);
+    })
+    .catch((error) => {
+      console.error("Error retrieving and sorting comments:", error.message);
+    });
 }
+
+function displaySortedComments(sortedComments) {
+  const commentsContainer = document.querySelector("#comments_dynamic");
+  commentsContainer.innerHTML = "";
+  sortedComments.forEach((comment) => {
+    const commentDiv = document.createElement("div");
+    commentDiv.classList.add("comments_dynamic");
+    commentDiv.innerHTML =
+      `<p>
+          <strong>${comment.name}</strong> - ${comment.dateTime}
+      </p>
+      <p>
+          <em>${comment.comment}</em>
+      </p>`;
+    commentsContainer.appendChild(commentDiv);
+  });
+}
+
 
